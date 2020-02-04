83e04d9c78d7a4d7375b0767a57765a742675a17
Vadim Korostelev <vdmkbu@ya.ru>
2020-02-04 20:43:15 +0500

    управление пользователями

--- app/User.php
+++ app/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'role', 'status'
     ];
 
     /**
@@ -42,4 +42,65 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function rolesList(): array
+    {
+        return [
+            self::ROLE_ADMIN => 'Admin',
+            self::ROLE_USER => 'User'
+        ];
+    }
+
+    public static function statusesList(): array
+    {
+        return [
+            self::STATUS_ACTIVE => 'Active',
+            self::STATUS_DISABLED => 'Disabled'
+        ];
+    }
+
+    public function isAdmin()
+    {
+        return $this->role === self::ROLE_ADMIN;
+    }
+
+    public function isDisabled()
+    {
+        return $this->status === self::STATUS_DISABLED;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->status === self::STATUS_ACTIVE;
+    }
+
+    // добавление пользователя
+    public static function register(string $name, string $email, string $password): self
+    {
+
+        return static::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt($password),
+            'role' => self::ROLE_USER,
+            'status' => self::STATUS_ACTIVE
+        ]);
+    }
+
+    // перключение статуса пользователя
+    public function switchStatus(): void
+    {
+        $this->update(['status' => $this->isActive() ? self::STATUS_DISABLED : self::STATUS_ACTIVE]);
+    }
+
+    public function changeRole($role): void
+    {
+        if (!in_array($role, [self::ROLE_USER, self::ROLE_ADMIN], true)) {
+            throw new \InvalidArgumentException('Undefined role "' . $role . '"');
+        }
+        if ($this->role === $role) {
+            throw new \DomainException('Role is already assigned.');
+        }
+        $this->update(['role' => $role]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,21 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group([
+    'prefix' => 'admin',
+    'as' => 'admin.',
+    'namespace' => 'Admin',
+    'middleware' => 'can:admin-panel'], function() {
+
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('users', 'UsersController');
+    Route::post('/users/{user}/switch', 'UsersController@switch')->name('users.switch');
+});
