50210397565254e7b69a8a9649457ee2a5e17d7c
Vadim Korostelev <vdmkbu@ya.ru>
2020-02-08 16:53:51 +0500

    установка библиотеки DOMPDF

---
+++ resources/views/projects/report/pdf.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<head>
+    <style type="text/css">
+        body {
+            font-family: DejaVu Sans;
+        }
+        p {
+            font-size: 13px;
+            word-break: break-all;
+            line-height: 1;
+
+        }
+
+        h2 {
+            font-size: 15px;
+            color: #999;
+        }
+
+
+
+        div.row {
+            width: 100%;
+            clear: both;
+
+        }
+
+        .wrapper {
+            width: 120px;
+            height: 120px;
+            float: left;
+            margin-left: 3px;
+        }
+        .exmpl img {
+            object-fit: cover;
+            width: 100%;
+            height: 100%;
+
+        }
+    </style>
+</head>
+<body>
+
+</body>
+</html>
